60d5e083b491d9a58613cef6a35303a241649d14
MomoPewpew <50401575+MomoPewpew@users.noreply.github.com>
2023-04-10 20:43:45 +0200

    Commented out interpolation code

--- src/main/java/com/momo/dvzchatbubbles/DvZChatBubbles.java
+++ src/main/java/com/momo/dvzchatbubbles/DvZChatBubbles.java
@@ -15,9 +15,6 @@ import org.bukkit.event.Listener;
 import org.bukkit.event.player.AsyncPlayerChatEvent;
 import org.bukkit.plugin.java.JavaPlugin;
 import org.bukkit.scheduler.BukkitRunnable;
-import org.bukkit.util.Transformation;
-import org.joml.AxisAngle4f;
-import org.joml.Vector3f;
 
 public class DvZChatBubbles extends JavaPlugin implements Listener {
 
@@ -82,8 +79,8 @@ public class DvZChatBubbles extends JavaPlugin implements Listener {
     	private Player player;
     	private String message;
     	private TextDisplay display = null;
-    	private Location prevLocation;
-    	private Location predLocation;
+/*    	private Location prevLocation;
+    	private Location predLocation;*/
 
     	public ChatBubble(Player player, String message) {
     		this.player = player;
@@ -91,20 +88,25 @@ public class DvZChatBubbles extends JavaPlugin implements Listener {
     	}
 
     	public void spawnChatBubble() {
-    		this.prevLocation = this.player.getLocation().clone();
+/*    		this.prevLocation = this.player.getLocation().clone();
     		this.prevLocation.setY(this.prevLocation.getY() + 2);
     		this.prevLocation.setPitch(0);
     		this.prevLocation.setYaw(0);
     		this.predLocation = this.prevLocation.clone();
 
-    		this.display = this.player.getWorld().spawn(this.prevLocation, TextDisplay.class);
+    		this.display = this.player.getWorld().spawn(this.prevLocation, TextDisplay.class);*/
+
+    		Location location = this.player.getLocation().clone();
+    		location.setY(location.getY() + 2);
+
+    		this.display = this.player.getWorld().spawn(location, TextDisplay.class);
     		this.display.setText(this.message);
 
     		this.display.setBillboard(Billboard.CENTER);
     		this.display.setLineWidth(150);
     		this.display.setSeeThrough(false);
     		this.display.setDefaultBackground(false);
-    		this.display.setInterpolationDuration(1);
+    		//this.display.setInterpolationDuration(1);
     		this.display.setShadowed(true);
     		this.display.setBrightness(new Brightness(15, 15));
     	}
@@ -115,6 +117,12 @@ public class DvZChatBubbles extends JavaPlugin implements Listener {
 
     	public void updatePosition() {
     		if (this.display != null && this.player != null && this.player.isValid()) {
+    			Location location = this.player.getLocation().clone();
+        		location.setY(location.getY() + 2);
+    			this.display.teleport(location);
+    		}
+    		//The commented out code was intended for use with Display interpolation. I commented it out because it turns out that as of right now, it is useless for my purpose.
+/*    		if (this.display != null && this.player != null && this.player.isValid()) {
         		Location currLocation = this.player.getLocation().clone();
         		currLocation.setY(currLocation.getY() + 2);
         		currLocation.setPitch(0);
@@ -133,7 +141,7 @@ public class DvZChatBubbles extends JavaPlugin implements Listener {
     			this.display.setTransformation(new Transformation(change, new AxisAngle4f(), new Vector3f(1), new AxisAngle4f()));
 
     			this.prevLocation = currLocation;
-    		}
+    		}*/
     	}
     }
 }
