b445013da572a5f3064424ec2051155a28745e65
MomoPewpew <50401575+MomoPewpew@users.noreply.github.com>
2023-04-10 21:56:37 +0200

    ChatBubbles use addPassenger to track players now

--- src/main/java/com/momo/dvzchatbubbles/DvZChatBubbles.java
+++ src/main/java/com/momo/dvzchatbubbles/DvZChatBubbles.java
@@ -15,6 +15,9 @@ import org.bukkit.event.Listener;
 import org.bukkit.event.player.AsyncPlayerChatEvent;
 import org.bukkit.plugin.java.JavaPlugin;
 import org.bukkit.scheduler.BukkitRunnable;
+import org.bukkit.util.Transformation;
+import org.joml.AxisAngle4f;
+import org.joml.Vector3f;
 
 public class DvZChatBubbles extends JavaPlugin implements Listener {
 
@@ -51,24 +54,17 @@ public class DvZChatBubbles extends JavaPlugin implements Listener {
 
         // Create the display entity and then track it to the players location
         new BukkitRunnable() {
-        	int count = 0;
-        	int duration = Math.max(message.length() * 2, 60);
-
             public void run() {
-            	if (count == 0) {
-                	chatBubble.spawnChatBubble();
-            	}
-
-                if (count < duration) {
-                	chatBubble.updatePosition();
-                	count++;
-                } else {
-                	chatBubble.removeChatBubble();
-                    cancel();
-                }
+            	chatBubble.spawnChatBubble();
             }
-        }.runTaskTimer(this, 0, 1);
-        bubbleList.remove(chatBubble);
+        }.runTask(this);
+
+        new BukkitRunnable() {
+            public void run() {
+            	chatBubble.removeChatBubble();
+                bubbleList.remove(chatBubble);
+            }
+        }.runTaskLater(this, Math.max(message.length(), 60));
     }
 
     public static List<ChatBubble> getBubbleList() {
@@ -79,8 +75,6 @@ public class DvZChatBubbles extends JavaPlugin implements Listener {
     	private Player player;
     	private String message;
     	private TextDisplay display = null;
-/*    	private Location prevLocation;
-    	private Location predLocation;*/
 
     	public ChatBubble(Player player, String message) {
     		this.player = player;
@@ -88,16 +82,8 @@ public class DvZChatBubbles extends JavaPlugin implements Listener {
     	}
 
     	public void spawnChatBubble() {
-/*    		this.prevLocation = this.player.getLocation().clone();
-    		this.prevLocation.setY(this.prevLocation.getY() + 2);
-    		this.prevLocation.setPitch(0);
-    		this.prevLocation.setYaw(0);
-    		this.predLocation = this.prevLocation.clone();
-
-    		this.display = this.player.getWorld().spawn(this.prevLocation, TextDisplay.class);*/
-
     		Location location = this.player.getLocation().clone();
-    		location.setY(location.getY() + 2);
+    		location.setY(location.getY() + this.player.getEyeHeight() * 0.85);
 
     		this.display = this.player.getWorld().spawn(location, TextDisplay.class);
     		this.display.setText(this.message);
@@ -106,42 +92,18 @@ public class DvZChatBubbles extends JavaPlugin implements Listener {
     		this.display.setLineWidth(150);
     		this.display.setSeeThrough(false);
     		this.display.setDefaultBackground(false);
-    		//this.display.setInterpolationDuration(1);
     		this.display.setShadowed(true);
     		this.display.setBrightness(new Brightness(15, 15));
+
+            this.display.setInterpolationDuration(0);
+    		this.display.setInterpolationDelay(-1);
+			this.display.setTransformation(new Transformation(new Vector3f(0F,0.8F,0F), new AxisAngle4f(), new Vector3f(1), new AxisAngle4f()));
+
+			this.player.setPassenger(this.display);
     	}
 
     	public void removeChatBubble() {
     		this.display.remove();
-    	}
-
-    	public void updatePosition() {
-    		if (this.display != null && this.player != null && this.player.isValid()) {
-    			Location location = this.player.getLocation().clone();
-        		location.setY(location.getY() + 2);
-    			this.display.teleport(location);
-    		}
-    		//The commented out code was intended for use with Display interpolation. I commented it out because it turns out that as of right now, it is useless for my purpose.
-/*    		if (this.display != null && this.player != null && this.player.isValid()) {
-        		Location currLocation = this.player.getLocation().clone();
-        		currLocation.setY(currLocation.getY() + 2);
-        		currLocation.setPitch(0);
-        		currLocation.setYaw(0);
-
-                Vector3f change = new Vector3f(
-            		(float) (currLocation.getX() - this.prevLocation.getX()),
-            		0F,
-            		(float) (currLocation.getZ() - this.prevLocation.getZ())
-        		);
-
-    			this.display.teleport(this.predLocation);
-                this.predLocation = currLocation.clone().add(change.x, change.y, change.z);
-
-        		this.display.setInterpolationDelay(-1);
-    			this.display.setTransformation(new Transformation(change, new AxisAngle4f(), new Vector3f(1), new AxisAngle4f()));
-
-    			this.prevLocation = currLocation;
-    		}*/
     	}
     }
 }
